0f328ca879db7cb988e86510866393c8b310a8e7
Long Nhat <nhathuynh227@gmail.com>
2025-01-09 13:52:50 +0700

    fix 2

--- src/server.js
+++ src/server.js
@@ -62,4 +62,6 @@ app.use((req, res) => {
   return res.status(404).send("404 not found");
 });
 
-export default app;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
